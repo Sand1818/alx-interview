9c4d19784cea9cd3de63125a037b959100e01939
Sand1818 <sandiso.jele@gmail.com>
2024-08-13 16:46:54 +0200

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+"""Check if boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    keys = []
+    keys.append(0)
+    for box in range(len(boxes)):
+        for key in boxes[box]:
+            if key not in keys:
+                if key != box and key < len(boxes):
+                    keys.append(key)
+    if len(keys) == len(boxes):
+        return True
+    return False
