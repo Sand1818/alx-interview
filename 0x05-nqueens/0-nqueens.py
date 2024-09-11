02a5b0c6b564665f34740efcf4754397ccdfc7d0
Sand1818 <sandiso.jele@gmail.com>
2024-09-11 02:02:05 +0200

    0-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,32 +6,41 @@ import sys
 def isSafe(Board, Row, Column):
     """ Checks if queen can move into column on the board"""
     for j in range(Row):
-        if (Board[j] == Column or 
-                abs(Board[j] - Column) == abs(Row - j)):
+        if Board[j] == Column or abs(Board[j] - Column) == Row - j:
             return False
     return True
 
-def populate_row(Board, Line):
-    """ Populates the chess board """
-    for i in range(len(Board)):
-        if isSafe(Board, Line, i):
-            Board[Line] = i
-            if Line < len(Board) - 1:
-                populate_row(Board, Line + 1)
-            else:
-                print([[i, Board[i]] for i in range(len(Board))])
+def solve_nqueens(N, board, row, sol):
+    if row == N:
+        sol.append(board[:])
+        return
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be an integer (number)")
-    sys.exit(1)
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    for col in range(N):
+        if isSafe(board, row, col):
+            board[row] = col
+            solve_nqueens(N, board, row + 1, sol)
 
-Board = [-1 for i in range(n)]
-populate_row(Board, 0)
+def print_Solution(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+    solve_nqueens(N, board, 0, solutions)
+
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(N)])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    N = sys.argv[1]
+    print_Solution(N)
